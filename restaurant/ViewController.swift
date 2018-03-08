da51494209f5ef3817aa6c3a3bc88e1fa8aec3e2
Jan Marten Sevenster <janmarten@wcw-stud-145-109-0-163.wireless.uva.nl>
2018-03-08 12:19:24 +0100

    Initial Commit

---
+++ restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  restaurant
+//
+//  Created by Jan Marten Sevenster on 08/03/2018.
+//  Copyright © 2018 John Appleseed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
